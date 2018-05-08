633644fad768e2905c77c6e02357a88da30ba292
Alber Erre <arblugo@gmail.com>
2018-05-08 22:39:39 +0100

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,11 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var PoolParty_token = artifacts.require("./PoolParty_token.sol");
+var PoolParty_Payroll = artifacts.require("./PoolParty_Payroll.sol");
+var utils = artifacts.require("./utils.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(PoolParty_token);
+  deployer.deploy(PoolParty_Payroll);
+  deployer.deploy(utils);
 };
